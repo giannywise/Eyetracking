a2016655fdbdcbb455cf90c2ccd1bc13d9ba27e8
giannywise <90458284+giannywise@users.noreply.github.com>
2022-06-10 17:45:11 +0200

    export eingerichtet, jetzt nur noch video laufen lassen

--- et_script.py
+++ et_script.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 import numpy as np
 import pandas as pd
 import os
+from os import startfile
 
 # Build tracker client, to establish a communication with the tracker server (an Eyeware application).
 #
@@ -24,41 +25,57 @@ import os
 # However, it is possible to set a specific hostname and port, depending on your setup and network.
 # See the TrackerClient API reference for further information.
 tracker = TrackerClient()
-start_time = time.time()  # Timer in Sekunden
 
+# Name und Datum für den späteren Export
+name = input("Enter Name_Datum:")
+
+# Startet das Video (Vorher Video öffnen)
+startfile("Conjuring2_0.mp4")
+
+# Timer in Sekunden
+start_time = time.time()
+
+# Timer wie lang das Skript läuft, orientiert an der Videolänge
+Timeout = time.time() + (3.31 * 60)
+
+# Löscht den alten DataFrame, wenn vorhanden
 try:
     del df
 except NameError:
     pass
 
-df = pd.DataFrame({'Gaze X': [], 'Gaze Y': [], 'Timestamp': []})  # DataFrame
+df = pd.DataFrame({'Timestamp': [], 'Gaze X': [], 'Gaze Y': [], "Lost Track": [], "Confidence": []})  # DataFrame
 
-# Run forever, until we press ctrl+c
-for _ in range(10):
-    # Make sure that the connection with the tracker server (Eyeware application) is up and running.
-    if tracker.connected:
+# Run forever, until we press ctrl+c/Strg + F2
+try:
+    while time.time() < Timeout:
+        # Make sure that the connection with the tracker server (Eyeware application) is up and running.
+        if tracker.connected:
 
-        print("  * Gaze on Screen:")
-        screen_gaze = tracker.get_screen_gaze_info()
-        screen_gaze_is_lost = screen_gaze.is_lost
-        print("      - Lost track:       ", screen_gaze_is_lost)
-        if not screen_gaze_is_lost:
-            print("      - Coordinates:       <x=%5.3f px,   y=%5.3f px>" % (screen_gaze.x, screen_gaze.y))
-            print("      - Confidence:       ", screen_gaze.confidence)
-            # print("      - Timestamps:       ", screen_gaze.confidence)
-            print("      - Timestamps:          %s     " % (time.time() - start_time))
-            df = df.append({"Gaze X": "%5.3f" % (screen_gaze.x), 'Gaze Y': "%5.3f" % (screen_gaze.y),
-                            'Timestamp': "%s" % (time.time() - start_time)}, ignore_index=True)
+            print("  * Gaze on Screen:")
+            screen_gaze = tracker.get_screen_gaze_info()
+            screen_gaze_is_lost = screen_gaze.is_lost
+            print("      - Lost track:       ", screen_gaze_is_lost)
+            if screen_gaze_is_lost:
+                df = df.append({"Lost Track": screen_gaze_is_lost, 'Timestamp': "%s" % (time.time() - start_time)}, ignore_index=True)
+            if not screen_gaze_is_lost:
+                print("      - Coordinates:       <x=%5.3f px,   y=%5.3f px>" % (screen_gaze.x, screen_gaze.y))
+                print("      - Confidence:       ", screen_gaze.confidence)
+                print("      - Timestamps:          %s     " % (time.time() - start_time))
+                df = df.append({'Timestamp': "%s" % (time.time() - start_time), "Gaze X": "%5.3f" % (screen_gaze.x),
+                                'Gaze Y': "%5.3f" % (screen_gaze.y),
+                                "Confidence": screen_gaze.confidence}, ignore_index=True)
 
-        time.sleep(1 / 60)  # We expect tracking data at 60 Hz
-    else:
-        # Print a message every MESSAGE_PERIOD_IN_SECONDS seconds
-        MESSAGE_PERIOD_IN_SECONDS = 2
-        time.sleep(MESSAGE_PERIOD_IN_SECONDS - time.monotonic() % MESSAGE_PERIOD_IN_SECONDS)
-        print("No connection with tracker server")
+            time.sleep(1 / 120)  # We expect tracking data at 120 Hz
+        else:
+            # Print a message every MESSAGE_PERIOD_IN_SECONDS seconds
+            MESSAGE_PERIOD_IN_SECONDS = 2
+            time.sleep(MESSAGE_PERIOD_IN_SECONDS - time.monotonic() % MESSAGE_PERIOD_IN_SECONDS)
+            print("No connection with tracker server")
+except KeyboardInterrupt:
+    pass
 
 print(df)
-name = input("Name eintragen:")
 
 # Löscht csv Dateien mit denselben Namen
 if os.path.exists(name + ".csv"):
