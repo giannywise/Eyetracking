c9770513cffe3c59a788f6cad6c464c4ebaf6c58
giannywise <giannysbusiness@gmail.com>
2022-07-02 17:49:39 +0200

    Done

--- DL_Model.py
+++ DL_Model.py
@@ -1,5 +1,6 @@
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 
 df_train = pd.read_csv('train/train.csv')
 df_test1 = pd.read_csv('test/test_1.csv')
@@ -14,9 +15,11 @@ y = df_train['Label']
 
 clf = MLPClassifier(solver='sgd', alpha=0.10, random_state=1, max_iter=2000, learning_rate_init=0.01)
 clf.fit(X, y)
-#MLPClassifier(alpha=1e-05, hidden_layer_sizes=(5, 2), random_state=1,solver='sdg')
 
 print(clf.predict(df_test1))
 print(clf.predict(df_test0))
 
 ### Anwendung von Cross Validation
+clf = MLPClassifier(solver='sgd', alpha=0.10, random_state=1, max_iter=2000, learning_rate_init=0.01)
+scores = cross_val_score(clf, X, y, cv=5)
+print(scores.mean())
