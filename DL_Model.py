b5371df0d44b2333dff9a24c17bac26b54f5062f
giannywise <90458284+giannywise@users.noreply.github.com>
2022-07-20 23:27:24 +0200

    Das modell wurde erweitert durch KNN und SVM

--- DL_Model.py
+++ DL_Model.py
@@ -1,25 +1,45 @@
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from sklearn.model_selection import cross_val_score
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
-df_train = pd.read_csv('train/train.csv')
+df_train = pd.read_csv('feature_final2_test.csv')
+#df_train = pd.read_csv('train/train.csv')
 df_test1 = pd.read_csv('test/test_1.csv')
 df_test0 = pd.read_csv('test/test_0.csv')
+feature = ['Anzahl Sakkaden',"Gesamt Dauer Sakkaden", 'Anzahl Fixationen', 'Anzahl Lost Tracks', 'Gesamt Dauer Fixationen', 'Dauer Lost Tracks']
 
-df_test1 = df_test1[['Anzahl Sakkaden','Anzahl Fixationen','Anzahl Lost Tracks','Gesamt Dauer Fixationen']]
-df_test0 = df_test0[['Anzahl Sakkaden','Anzahl Fixationen','Anzahl Lost Tracks','Gesamt Dauer Fixationen']]
+df_test1 = df_test1[feature]
+df_test0 = df_test0[feature]
 
-X = df_train[['Anzahl Sakkaden','Anzahl Fixationen','Anzahl Lost Tracks','Gesamt Dauer Fixationen']]
+X = df_train[feature]
 
 y = df_train['Label']
 
-clf = MLPClassifier(solver='sgd', alpha=0.10, random_state=1, max_iter=2000, learning_rate_init=0.01,hidden_layer_sizes= 10)
-clf.fit(X, y)
+### Stochastic Gradient Decent
+clf_sdg = MLPClassifier(solver='sgd', random_state=1, max_iter=2000, learning_rate_init=0.01, hidden_layer_sizes=6)
+clf_sdg.fit(X, y)
+print("NN SDG: ",clf_sdg.predict(df_test1))
+print("NN SDG: ",clf_sdg.predict(df_test0))
 
-print(clf.predict(df_test1))
-print(clf.predict(df_test0))
+### SVM
+clf_svm = svm.SVC(C=2)
+clf_svm.fit(X, y)
+print("SVM: ", clf_svm.predict(df_test1))
+print("SVM: ", clf_svm.predict(df_test0))
+
+### KNN
+clf_knn = KNeighborsClassifier(n_neighbors=1)
+clf_knn.fit(X, y)
+print("KNN: ", clf_knn.predict(df_test1))
+print("KNN: ", clf_knn.predict(df_test0))
 
 ### Anwendung von Cross Validation
-clf = MLPClassifier(solver='sgd', alpha=0.10, random_state=1, max_iter=2000, learning_rate_init=0.01, hidden_layer_sizes= 10)
-scores = cross_val_score(clf, X, y, cv=5)
-print(scores.mean())
+scores_sdg = cross_val_score(clf_sdg, X, y, cv=5)
+scores_svm = cross_val_score(clf_svm, X, y, cv=5)
+scores_knn = cross_val_score(clf_knn, X, y, cv=5)
+print("Cross_Val_Score NN SDG: ", scores_sdg.mean())
+print("Cross_Val_Score SVM: ", scores_svm.mean())
+print("Cross_Val_Score KNN: ", scores_knn.mean())
